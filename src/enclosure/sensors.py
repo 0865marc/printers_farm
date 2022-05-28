3a1f4a95e03e94fd10dd2b0c69068497e29d191e
Marc Teixidó <0865marc@gmail.com>
2022-05-28 21:21:32 +0200

    Correction in sensors.py and requirements.txt updated

--- src/enclosure/sensors.py
+++ src/enclosure/sensors.py
@@ -1,26 +1,41 @@
 from mqtt.mqtt_publisher import Publisher_Mqtt
-
+import random
 
 class Sensor(object):
     def __init__(self):
         self.publisher = Publisher_Mqtt()
         
-
-    def publish_message(self):
-        if isinstance(self, SensorTemperatura):
-            self.publisher.client.publish("sensors/temperature", self.value)
+    def publish_message(self, topic, payload):
+            self.publisher.client.publish(topic, payload)
 
 
-class SensorTemperatura(Sensor):
+class TemperatureSensor(Sensor):
     def __init__(self):
         Sensor.__init__(self)
-
     def random(self):
-        self.value = 32
+        self.value = random.randint(30,50)
+    def send(self):
+        self.publish_message("sensors/temperature", self.value)
+
+class HumiditySensor(Sensor):
+    def __init__(self):
+        Sensor.__init__(self)
+    def random(self):
+        self.value = random.randint(30,50)
+    def send(self):
+        self.publish_message("sensors/humidity", self.value)
+
+class NoiseSensor(Sensor):
+    def __init__(self):
+        Sensor.__init__(self)
+    def random(self):
+        self.value = random.randint(30,50)
+    def send(self):
+        self.publish_message("sensors/noise", self.value)
 
 
 # Testing
-a = SensorTemperatura()
+a = TemperatureSensor()
 print("Generate random data")
 a.random()
 a.publish_message()
