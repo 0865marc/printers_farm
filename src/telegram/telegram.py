d72b665c9654a54a05657520b5830c36b85bdffe
Marc Teixidó <0865marc@gmail.com>
2022-05-28 09:59:23 +0200

    Added basic schema of telegram

---
+++ src/telegram/telegram.py
@@ -0,0 +1,15 @@
+import telepot
+from telepot.loop import MessageLoop
+
+"""""
+class Bot():
+    def __init__(self, token):
+        pass
+"""
+
+
+# Test the account
+BOT_TOKEN = "HARD-CODED TOKEN"
+
+bot = telepot.Bot()
+bot.getMe()
