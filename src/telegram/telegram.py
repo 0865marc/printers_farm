ec9117bcdde4cace09ebc6be7889ddded73458e0
Marc Teixidó <0865marc@gmail.com>
2022-05-28 15:26:00 +0200

    Added basic functionality of Bot class

--- src/telegram/telegram.py
+++ src/telegram/telegram.py
@@ -1,15 +1,34 @@
-import telepot
+from email import message
+import telepot                                       # https://telepot.readthedocs.io/en/latest/
 from telepot.loop import MessageLoop
+import time
 
-"""""
-class Bot():
+BOT_TOKEN = "5529655394:AAGN9hGkbrhcVYvq_EWYQzag4P5Y7q56DBs"
+
+class Bot(object):
+
     def __init__(self, token):
-        pass
-"""
+        self.bot = telepot.Bot(BOT_TOKEN)
+        MessageLoop(self.bot, self.handle).run_as_thread()
+        print("Listening......")
+        while True:
+            time.sleep(10)
+
+    def handle(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+
+        if content_type == "text":
+            command = msg["text"]
+            print("Command recieved: ", command)
+
+            if command == "/something":
+                pass
+            elif command == "/somethingDifferent":
+                pass
+            else:
+                print("Unknown command")
 
 
-# Test the account
-BOT_TOKEN = "HARD-CODED TOKEN"
 
-bot = telepot.Bot()
-bot.getMe()
+if __name__ == "__main__":
+    farm_Bot = Bot(BOT_TOKEN)
